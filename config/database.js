dc12395f4ecd31ce2de37162e235a30dc18f63dd
highfivenick <nickhawkins.dev@gmail.com>
2022-05-30 21:11:46 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://highfive_nick:addabar@cluster0.4n9lz.mongodb.net/script?retryWrites=true&w=majority', 
+    'dbName': 'script'
+};
